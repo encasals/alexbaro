787f449515690e20b06000bd70f4a4c7af6bfd3b
Enric Casals <enric.casals@hp.com>
2026-05-06 11:52:34 +0200

    Update site URL to alexbaro.com for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alexbaro.vercel.app',
+  site: 'https://alexbaro.com',
   integrations: [
     sitemap({
       changefreq: 'weekly',
